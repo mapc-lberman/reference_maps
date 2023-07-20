6392dd5a74e6e1dae7d6c6c5c7808dd0344e427c
mapc-lberman <lberman@mapc.org>
2023-07-20 18:45:31 -0400

    crs transform update

--- crs_transform_in_R/crs_transform.R
+++ crs_transform_in_R/crs_transform.R
@@ -10,14 +10,31 @@ setwd("K:/DataServices//Datasets/Boundaries/Spatial")
 # set input paths to vector files
 mapc_twn_orig <- st_read("mapc_towns_poly.shp") 
 
+mapc_border_orig <- st_read("mapc_outline_poly.shp") 
+
+
+
 #check crs and units (meters)
 st_crs(mapc_twn_orig)
+
+st_crs(mapc_border_orig)
 
 #transform to wgs84
 mapc_twn_4326 <- st_transform(mapc_twn_orig, crs = 4326)
 
+mapc_border_4326 <- st_transform(mapc_border_orig, crs = 4326)
+
+
 #check output
 st_crs(mapc_twn_4326)
 
+st_crs(mapc_border_4326)
+
+
 #export 
 st_write(mapc_twn_4326, "mapc_towns_epsg_4326.shp")
+
+st_write(mapc_border_4326, "mapc_outline_epsg_4326.shp")
+
+#export to geojson
+#st_write(mapc_twn_4326, "mapc_towns_epsg_4326.geojson") 
